86ab3c644db1081aeccf26f936d0e193a2b1f836
Connor White <connor.bcw@gmail.com>
2023-01-10 20:01:05 -0600

    feat!: migrate to ESM and add isTSConfigJSON, parseTSConfigJSON

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,36 @@
 import { test, expect } from "@jest/globals";
+import { fileName, isTSConfigJSON, parseTSConfigJSON } from "../source/index.js";
 
-import { fileName } from "../source";
+const tsconfig = {
+  $schema: "https://json.schemastore.org/tsconfig",
+  extends: "@autosoft/tsconfig",
+  compilerOptions: {
+    outDir: ".auto/build",
+    rootDirs: [
+      "source"
+    ]
+  },
+  files: [
+    "source/index.ts"
+  ]
+};
 
-test("import sanity check", () => {
+test("fileName", () => {
   expect(fileName).toBe("tsconfig.json");
+});
+
+test("isTSConfigJSON", () => {
+  expect(isTSConfigJSON(tsconfig)).toBe(true);
+  expect(isTSConfigJSON({
+    ...tsconfig,
+    invalid_key: true
+  })).toBe(false);
+});
+
+test("parseTSConfigJSON", () => {
+  expect(parseTSConfigJSON(tsconfig)).toEqual(tsconfig);
+  expect(parseTSConfigJSON({
+    ...tsconfig,
+    invalid_key: true
+  })).toBeUndefined();
 });
